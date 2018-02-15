c5751787dca6c09067907a1ce4770901b26469f5
Ghassanooooo <ghassanaldarwish@yahoo.com>
2018-02-15 11:51:48 +0100

    index html , style css, img and script js files

---
+++ VulkanClubProject/java/submit.js
@@ -0,0 +1,11 @@
+
+var text1 = "Good morning";
+var text2 = " John";
+var x = 2;
+var y = 3;
+var z = x+y;
+alert(text1+text2);
+alert("2 + 3 ist " + z);
+
+var name = prompt("Please enter your name");
+document.write("Your name is " + name);
